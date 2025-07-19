8624159556aeddfdd7729c807678f5b699f21fd3
Apex Unified <apexunified@gmail.com>
2025-07-19 13:59:30 +0500

    Updated

--- client/src/pages/Auth/Login.jsx
+++ client/src/pages/Auth/Login.jsx
@@ -122,6 +122,7 @@ const Login = () => {
             disabled={isSubmitting || loading}
             className="flex justify-center w-full gap-3 px-4 py-2 text-sm font-medium text-white border border-transparent rounded-md shadow-sm bg-gradient-to-r from-blue-600 to-purple-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 disabled:opacity-50 disabled:cursor-not-allowed"
           >
+            Sign in
             {isSubmitting && (
               <>
                 <div role="status">
@@ -144,7 +145,6 @@ const Login = () => {
                 </div>
               </>
             )}
-            Sign in
           </button>
         </div>
       </form>
